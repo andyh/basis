9159de23ff579e5fc8525e1de0796cb4adb206a9
John Barnette <jbarnette@gmail.com>
2008-11-12 20:14:32 -0800

    Basic template cache path and config.

--- lib/basis/template.rb
+++ lib/basis/template.rb
@@ -1,12 +1,25 @@
+require "digest/sha1"
 require "uri"
+require "yaml"
 
 module Basis
   class Template
-    attr_reader :source
+    attr_reader :cache, :source
 
     def initialize(repository, source)
       @repository = repository
       @source = source
+      
+      @cache = repository.path + "#{File.basename(source)}-#{Digest::SHA1.hexdigest(source)}"
+    end
+    
+    def cached?
+      @cache.exist?
+    end
+    
+    def config
+      config = @cache + "basis" + "config.yml"
+      config.exist? ? YAML.load(config.read) : {}
     end
   end
 end
--- test/basis/template_test.rb
+++ test/basis/template_test.rb
@@ -2,9 +2,27 @@ require "helper"
 
 module Basis
   class TemplateTest < Test::Unit::TestCase
-    def test_initialize_takes_a_source
-      t = Basis::Template.new(nil, "source")
-      assert_equal("source", t.source)
+    def setup
+      path = Dir.tmpdir + "/repo"
+      FileUtils.rm_rf path
+      FileUtils.mkdir path
+      
+      @repo = Basis::Repository.new(path)
+      @template = Basis::Template.new(@repo, "source")
+      FileUtils.cp_r FIXTURES_PATH + "/static", @template.cache
+    end
+
+    def test_initialize_takes_a_source      
+      assert_equal("source", @template.source)
+    end
+    
+    def test_loads_config_if_available
+      assert_equal("static!", @template.config["name"])
+    end
+    
+    def test_has_empty_config_if_not_available
+      t = Basis::Template.new(@repo, "whatever")
+      assert_equal({}, t.config)
     end
   end
 end
